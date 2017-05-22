5f19997e9d2fad385e3e291c9cf07d9286257101
Stefan Kuhn <stefan.kuhn@mailbox.org>
2017-05-22 15:30:45 +0200

    Add abbreviations for git

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,5 +15,22 @@ set fish_user_paths $fish_user_paths $HOME/.pyenv/bin
 if not set -q abbrs_initialized
     set -U abbrs_initialized
 
+    # Audible to mp3
     abbr -a audible 'bash AAXtoMP3 --single 9bb94d07'
+
+    # Pass
+    abbr -a pgp     'pass git push'
+    abbr -a p       'pass'
+
+    # Git
+    abbr -a g       'git'
+    abbr -a gs      'git status'
+    abbr -a gc      'git clone --recursive'
+    abbr -a ga      'git add'
+    abbr -a gch     'git checkout'
+    abbr -a gcb     'git checkout -b'
+    abbr -a grc     'git rm --cached'
+    abbr -a gf      'git fetch --all'
+    abbr -a gm      'git merge'
+    abbr -a gp      'git push'
 end
