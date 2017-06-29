45d33e517a8635617ca40b230e221e89975e345a
Stefan Kuhn <stefan.kuhn@mailbox.org>
2017-06-29 18:27:43 +0200

    Lock automatic screensaver only after 1 min delay

--- .config/fish/functions/i3exit.fish
+++ .config/fish/functions/i3exit.fish
@@ -1,11 +1,9 @@
 function i3exit
-  function lock
-      xscreensaver-command -lock
-  end
-
   switch $argv
       case lock
-          lock
+          xscreensaver-command -lock
+      case saver
+          xscreensaver-command -activate
       case logout
           i3-msg exit
       case suspend
@@ -17,7 +15,7 @@ function i3exit
       case shutdown
           systemctl poweroff
       case '*'
-          echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+          echo "Usage: $0 {lock|saver|logout|suspend|hibernate|reboot|shutdown}"
           exit 2
   end
 end
