5030982e049e91e8a8b9c164f245827e5470fd74
Stefan Kuhn <stefan.kuhn@mailbox.org>
2017-07-29 11:46:32 +0200

    Change theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -81,3 +81,11 @@ if not set -q __abbrs_initi
     # ranger
     abbr -a r       'ranger'
 end
+
+# Setup bobthefish theme
+set -g theme_display_vi yes
+set -g theme_display_cmd_duration yes
+set -g theme_powerline_fonts no
+set -g theme_color_scheme solarized-light
+set -g theme_nerd_fonts yes
+set -g theme_display_user yes
