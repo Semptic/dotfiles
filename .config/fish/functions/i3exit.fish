0cfbd83e47aa081a8f700e81f6729f5fa2ff0002
Stefan Kuhn <stefan.kuhn@mailbox.org>
2017-10-16 09:07:13 +0200

    Add random image for screensaver and (broken) hibernate/suspend

--- .config/fish/functions/i3exit.fish
+++ .config/fish/functions/i3exit.fish
@@ -1,13 +1,31 @@
+function lock
+    set IMG_PATH "$HOME/MyCloud/Photos/EarthPorn/"
+    set IMG (find $IMG_PATH -maxdepth 1 -type f -not -name "*.sh" | shuf -n 1)
+    trap revert_dpms HUP INT TERM
+    xset dpms 1800 1800 1800 
+    xset +dpms 
+    eval $HOME/.config/i3/scripts/i3lock-multimonitor/lock -i "$IMG" -a "-n"
+    revert_dpms
+end
+
+function revert_dpms
+    xset dpms 0 0 0
+end
+
 function i3exit
   switch $argv
       case lock
-          i3lock
+          lock
       case logout
           i3-msg exit
       case suspend
-          lock; and systemctl hybrid-sleep
+          fish -c 'i3exit lock' &
+          sleep 1
+          systemctl hybrid-sleep
       case hibernate
-          lock; and systemctl hibernate
+          fish -c 'i3exit lock' &
+          sleep 1
+          systemctl hibernate
       case reboot
           systemctl reboot
       case shutdown
