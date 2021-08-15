d021f904282a2266ff2beac8dc9f948ceee1d72e
Stefan Kuhn <semptic@kuhn.cloud>
2021-08-15 20:43:18 +0200

    Fix path to omf

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -11,8 +11,8 @@ end
 
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
+  and set -gx OMF_PATH "$XDG_DATA_HOME/oh-my-fish"
+  or set -gx OMF_PATH "$HOME/.local/share/oh-my-fish"
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
