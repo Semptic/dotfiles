58f48813bed2f05659f0f884fab728be4e33e90c
Stefan Kuhn <semptic@kuhn.cloud>
2021-08-26 15:37:43 +0200

    Disable fish ssh-agent

--- .config/fish/conf.d/plugins.fish
+++ .config/fish/conf.d/plugins.fish
@@ -1,6 +1,6 @@
 # Install automatically
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
-fundle plugin 'danhper/fish-ssh-agent'
+# fundle plugin 'danhper/fish-ssh-agent'
 
 fundle init
